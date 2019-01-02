e2f8a5ff48c4d90956a67de79d175f8c0d7294cd
joeywang4 <wangjoey4@gmail.com>
2019-01-02 22:16:06 +0800

    Update time_stretch.py

--- music_py/time_stretch.py
+++ music_py/time_stretch.py
@@ -16,28 +16,26 @@ def ola (data, s, N, H_a):
 
 def wsola (data, s, N, H_a):
     L = len(data)
-    delta_max = 128
+    delta_max = 256
     H_s = int (s*H_a)
 
     x = []
     for i in range(0, L-N-H_s-delta_max, H_a):
         if i == 0:
-            x.append(ori_data[0:N])
-            last_x = ori_data[i+H_s:i+H_s+N]
+            x.append(data[0:N])
+            last_x = data[i+H_s:i+H_s+N]
         else:
-            best_x = ori_data[i:i+N]
-            for j in range(-delta_max, delta_max, 1):
-                try_x = ori_data[i+j:i+j+N]
-                if sum(sum(last_x * try_x)) > sum(sum(last_x * best_x)):
-                    best_x = try_x
-                    index = j 
+            try_x = np.correlate(
+                data[i:i+N-H_s],
+                last_x)
+            index = np.argmax(try_x)
+            best_x = data[i+index:i+index+N]
             x.append(best_x)
-            last_x = ori_data[i+index+H_s:i+index+H_s+N]
+            last_x = data[i+index+H_s:i+index+H_s+N]
 
     x = np.array(x)
-    Hann_window = np.array([[0.5*(1-np.cos( 2*np.pi* (i+N//2) / (N-1) ))]*2 for i in range(-N//2, N//2)])
-
-    new_data = np.zeros(((H_s*len(x)+N-H_s),2))
+    Hann_window = np.array([0.5*(1-np.cos( 2*np.pi* (i+N//2) / (N-1) )) for i in range(-N//2, N//2)])
+    new_data = np.zeros((H_s*len(x)+N-H_s))
     for i in range( 0, len(x)):
         new_data[i*H_s:i*H_s+N] += x[i] * Hann_window
     
@@ -64,21 +62,28 @@ def vocoder (data, s, N, H_a, sample_rate):
     new_data = result.astype(np.int16)
     return new_data
 
-sample_rate, ori_data = wav.read('k_1.wav')
 
-s = 1.5 # stretching factor
-N = 512 # window_size
-H_a = 256 # original shift
+if __name__ == "__main__":
+    sample_rate, ori_data = wav.read('DAOKO.wav')
 
-#new_data = ola (ori_data, s, N, H_a)
-#new_data = wsola (ori_data, s, N, H_a)
+    s = 1.3 # stretching factor
+    N = 512 # window_size
+    H_a = 256 # original shift
 
-left  = vocoder (ori_data[:,0], s, N, H_a, sample_rate)
-right = vocoder (ori_data[:,1], s, N, H_a, sample_rate)
-new_data = np.zeros( (len(left), 2) , dtype = 'int16')
-new_data[:,0] = left
-new_data[:,1] = right
+    #new_data = ola (ori_data, s, N, H_a)
+    
+    left  = wsola (ori_data[:,0], s, N, H_a)
+    right = wsola (ori_data[:,1], s, N, H_a)
+    new_data = np.zeros( (len(left), 2) , dtype = 'int16')
+    new_data[:,0] = left
+    new_data[:,1] = right
 
-wav.write('test.wav', sample_rate, new_data)
+    #left  = vocoder (ori_data[:,0], s, N, H_a, sample_rate)
+    #right = vocoder (ori_data[:,1], s, N, H_a, sample_rate)
+    #new_data = np.zeros( (len(left), 2) , dtype = 'int16')
+    #new_data[:,0] = left
+    #new_data[:,1] = right
+
+    wav.write('test.wav', sample_rate, new_data)
 
 
